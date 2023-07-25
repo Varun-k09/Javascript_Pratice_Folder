e1acee506c8abf34b7f41eec5212fda9180a692e
Varun-k09 <80519882+Varun-k09@users.noreply.github.com>
2023-07-25 11:01:37 +0000

    varLetConst

---
+++ Bacics_Basics/varLetConst.js
@@ -0,0 +1,7 @@
+var accountId=14443
+let accountPassword="147852"
+let emailId="bethur@gmail.com"
+accountCity="davanagere"
+let accountState
+
+console.table([accountId, accountPassword, accountCity, emailId, accountState])
